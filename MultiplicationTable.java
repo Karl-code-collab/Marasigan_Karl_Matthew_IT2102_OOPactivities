8c3f19d7b546579a86126a9806f41e2006e6c881
Karlmrsgn <141891829+Karl-code-collab@users.noreply.github.com>
2024-11-28 20:10:03 +0800

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("Enter the size of the multiplication table: ");
+        int maxNumber = console.nextInt();
+
+        System.out.println("Multiplication Table: ");
+        for (int i = 1; i <= maxNumber; i++) {
+            for (int j = 1; j <= maxNumber; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
